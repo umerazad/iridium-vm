8c5fc751245349a7b329584b0668b306380c051b
Muhammad Umer Azad <umer@fastmail.com>
2019-08-30 17:03:17 -0700

    Graceful Ctrl-C handling + Basic CLI args.

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,24 @@ pub mod instruction;
 pub mod repl;
 pub mod vm;
 
+use ctrlc;
 use repl::REPL;
+use structopt::StructOpt;
+
+/// REPL for Iridium VM.
+#[derive(StructOpt, Debug)]
+struct Opt {}
 
 fn main() {
+    let _ = Opt::from_args();
+
+    // Gracefully handle the Ctrl-C stuff.
+    ctrlc::set_handler(|| {
+        println!("\nCtrl+C detected. Terminating.");
+        std::process::exit(1);
+    })
+    .expect("Error setting-up Ctrl-C handler.");
+
     let mut repl = REPL::new();
     repl.run();
 }
